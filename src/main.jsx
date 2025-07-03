450b9f81b3fa78ee7addb790aaa19f0ad0bf6b0f
sebastian <sebastianduvan.mp@gmail.com>
2025-07-03 11:54:51 -0400

    css solucionado

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
 
 /**
  * Punto de entrada principal de la aplicación React
